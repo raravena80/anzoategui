35cf61ec2086b73918805cc7a07c31651267d76d
Ricardo Aravena <raravena@branch.io>
2018-03-01 18:39:52 -0800

    Minor change to change bool

--- ownership/take.rs
+++ ownership/take.rs
@@ -4,10 +4,19 @@ fn take(v: Vec<i32>) {
     println!("v[0] is {}", v[0]);
 }
 
+fn change_truth(x: bool) -> bool {
+    !x
+}
+
 fn main() {
 
     let v = vec![1, 2, 3];
     take(v);
     // The following would fail
     //println!("v[0] is {}", v[0]);
+    //
+    let a = true;
+    let y = change_truth(a);
+    println!("{}", a);
+    println!("{}", y);
 }
