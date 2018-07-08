fe582c076af924c46e96ea899dc00cbfa02b152e
Ricardo Aravena <raravena@branch.io>
2018-07-07 22:32:09 -0700

    Use changes
    Signed-off-by: Ricardo Aravena <raravena@branch.io>

--- use/use.rs
+++ use/use.rs
@@ -2,18 +2,20 @@
 
 enum Status {
     Rich,
+    Middle,
     Poor,
 }
 
 enum Work {
     Civilian,
     Soldier,
+    Redcross,
 }
 
 fn main() {
     // Explicitly `use` each name so they are available without
     // manual scoping.
-    use Status::{Poor, Rich};
+    use Status::{Poor, Middle, Rich};
     // Automatically `use` each name inside `Work`.
     use Work::*;
 
@@ -25,6 +27,7 @@ fn main() {
     match status {
         // Note the lack of scoping because of the explicit `use` above.
         Rich => println!("The rich have lots of money!"),
+        Middle => println!("The middle class has some money!"),
         Poor => println!("The poor have no money..."),
     }
 
@@ -32,5 +35,6 @@ fn main() {
         // Note again the lack of scoping.
         Civilian => println!("Civilians work!"),
         Soldier  => println!("Soldiers fight!"),
+        Redcross => println!("Red Cross members help!"),
     }
 }
