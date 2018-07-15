9498ed063421c8ac723e1ab40ecfdc2661d072ca
Ricardo Aravena <raravena@branch.io>
2018-07-14 22:26:50 -0700

    Add turpial
    Signed-off-by: Ricardo Aravena <raravena@branch.io>

--- bounds/bounds.rs
+++ bounds/bounds.rs
@@ -1,27 +1,33 @@
 struct Cardinal;
 struct BlueJay;
+struct Turpial;
 struct Turkey;
 
 trait Red {}
 trait Blue {}
+trait Yellow {}
 
 impl Red for Cardinal {}
 impl Blue for BlueJay {}
+impl Yellow for Turpial {}
 
 // These functions are only valid for types which implement these
 // traits. The fact that the traits are empty is irrelevant.
 fn red<T: Red>(_: &T)   -> &'static str { "red" }
 fn blue<T: Blue>(_: &T) -> &'static str { "blue" }
+fn yellow<T: Yellow>(_: &T) -> &'static str { "yellow" }
 
 fn main() {
     let cardinal = Cardinal;
     let blue_jay = BlueJay;
+    let turpial = Turpial;
     let _turkey   = Turkey;
 
     // `red()` won't work on a blue jay nor vice versa
     // because of the bounds.
     println!("A cardinal is {}", red(&cardinal));
     println!("A blue jay is {}", blue(&blue_jay));
+    println!("A turpial is {}", yellow(&turpial));
     //println!("A turkey is {}", red(&_turkey));
     // ^ TODO: Try uncommenting this line.
 }
