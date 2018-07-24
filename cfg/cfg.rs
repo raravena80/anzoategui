c13acd6504af9ff3cc488f0826bd13b0b5e543a7
Ricardo Aravena <raravena@branch.io>
2018-07-24 14:18:47 -0700

    Add darwin
    Signed-off-by: Ricardo Aravena <raravena@branch.io>

--- cfg/cfg.rs
+++ cfg/cfg.rs
@@ -4,14 +4,25 @@ fn are_you_on_linux() {
     println!("You are running linux!");
 }
 
+#[cfg(target_os = "darwin")]
+fn are_you_on_mac() {
+    println!("You are running mac!");
+}
+
 // And this function only gets compiled if the target OS is *not* linux
 #[cfg(not(target_os = "linux"))]
 fn are_you_on_linux() {
     println!("You are *not* running linux!");
 }
 
+#[cfg(not(target_os = "darwin"))]
+fn are_you_on_mac() {
+    println!("You are running mac!");
+}
+
 fn main() {
     are_you_on_linux();
+    are_you_on_mac();
     
     println!("Are you sure?");
     if cfg!(target_os = "linux") {
