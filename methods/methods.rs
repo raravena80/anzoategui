ab8aa525f1c2e8b9c3fcaaa5412b5da3e940a8f7
Ricardo Aravena <raravena@branch.io>
2018-07-29 15:29:02 -0700

    Add perimeter
    Signed-off-by: Ricardo Aravena <raravena@branch.io>

--- methods/methods.rs
+++ methods/methods.rs
@@ -8,6 +8,10 @@ impl Rectangle {
     fn area(&self) -> u32 {
         self.width * self.height
     }
+
+    fn perimeter(&self) -> u32 {
+        (self.width * 2) + (self.height * 2)
+    }
 }
 
 fn main() {
@@ -16,5 +20,10 @@ fn main() {
     println!(
         "The area of the rectangle is {} square pixels.",
         rect1.area()
+    );
+
+    println!(
+        "The perimeter of the rectangle is {} pixels.",
+        rect1.perimeter()
     );
 }
