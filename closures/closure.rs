938bba6fcdc9bac4465f8d06a4e1050c156e5290
Ricardo Aravena <raravena@branch.io>
2018-09-22 13:57:28 -0700

    Adding to closure.rs
    Signed-off-by: Ricardo Aravena <raravena@branch.io>

--- closures/closure.rs
+++ closures/closure.rs
@@ -19,6 +19,8 @@ fn main() {
     // The return type is inferred.
     let one = || 1;
     println!("closure returning one: {}", one());
+    let string = || "Hello";
 
+    println!("closure returning string: {}", string());
 }
 
