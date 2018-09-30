30bc0956fa6d5ba56ec25e4b3f3553ca6ce97e12
Ricardo Aravena <raravena@branch.io>
2018-09-29 17:23:11 -0700

    Add float
    Signed-off-by: Ricardo Aravena <raravena@branch.io>

--- fromto/from.rs
+++ fromto/from.rs
@@ -5,13 +5,27 @@ struct Number {
     value: i32,
 }
 
+#[derive(Debug)]
+struct Float {
+    value: f32,
+}
+
 impl From<i32> for Number {
     fn from(item: i32) -> Self {
         Number { value: item }
     }
 }
 
+impl From<f32> for Float {
+    fn from(item: f32) -> Self {
+        Float { value: item }
+    }
+}
+
 fn main() {
     let num = Number::from(30);
     println!("My number is {:?}", num);
+
+    let float = Float::from(50.0);
+    println!("My float is {:?}", float);
 }
