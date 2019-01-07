05883951b8c3a8025ffcaca3d6cfb734a19d6fa4
Ricardo Aravena <raravena@branch.io>
2019-01-06 19:16:19 -0800

    Guards
    Signed-off-by: Ricardo Aravena <raravena@branch.io>

--- guards/guards.rs
+++ guards/guards.rs
@@ -20,5 +20,15 @@ fn main() {
         (x, _) if x % 2 == 1 => println!("The first one is odd"),
         _ => println!("No correlation..."),
     }
+
+    let pair3 = (3, 4);
+    println!("Tell me about {:?}", pair3);
+    match pair3 {
+        (x, y) if x == y => println!("These are twins"),
+        // The ^ `if condition` part is a guard
+        (x, y) if x + y == 0 => println!("Antimatter, kaboom!"),
+        (x, _) if x % 2 == 1 => println!("The first one is odd"),
+        _ => println!("No correlation..."),
+    }
 }
 
